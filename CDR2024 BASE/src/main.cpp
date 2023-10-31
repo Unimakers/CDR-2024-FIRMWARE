d87c74bd46c471363605ba43ce6d420116019547
ThatOneDragon <looijdylan@gmail.com>
2023-10-31 23:35:02 +0100

    Un test avec Ecran I2C

--- CDR2024 BASE/src/main.cpp
+++ CDR2024 BASE/src/main.cpp
@@ -1,20 +1,40 @@
 #include <Arduino.h>
 #include <UniBoardDef.h>
-
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
 // put function declarations here:
+
+SSD1306Wire display(0x3c); 
 
 void setup() {
   Serial.begin(9600);
   pinMode(TIR, INPUT_PULLUP);
+  Wire.begin(SDA,SCL);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+
+  Serial.println("Waiting for button press");
+  while(digitalRead(TIR)==1){
+
+  }
+  Serial.println("Start");
 }
 
 void loop() {
-  Serial.println("Bonjour Monde");  
+
   if(digitalRead(TIR)==0){
-    Serial.println("Pressed");
+    display.clear();
+    display.drawString(0, 0, "Pressed");
+    display.display();
   }else{
-    Serial.println("Not pressed");
+    display.clear();
+    display.drawString(0, 0, "N.pressed");
+    display.display();
   }
   delay(1000);
+
 }
 
